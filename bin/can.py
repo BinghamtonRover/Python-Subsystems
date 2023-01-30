61877fe66af4794b70cba7c24e64ccaf98268d9f
Levi Lesches <levilesches@gmail.com>
2023-01-30 11:42:22 -0500

    Cleanup

--- bin/can.py
+++ bin/can.py
@@ -1,13 +1,8 @@
 import lib.services as services
 
 service = services.CanService(test=True)
-# message_sender = services.MessageSender()
-# message_receiver = services.MessageReceiver()
 
 service.register_handler(1, handler=lambda x: print(x.data))
 service.mock_send(1, b"Hola")
-
-rover = services.Rover()
-rover.drive.brake()
 
 print("Done")
