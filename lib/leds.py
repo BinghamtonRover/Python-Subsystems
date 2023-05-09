ba279ce12ca06d3c47cd368d0859eca5738b942a
Levi Lesches <levilesches@gmail.com>
2023-05-09 10:46:12 -0400

    Fixed typos

--- lib/leds.py
+++ lib/leds.py
@@ -13,15 +13,20 @@ class Leds:
 	PWM_PIN = 17
 
 	def __init__(self): 
-		self.pi = pigpio.pi()
-		self.set_mode(RED_PIN, pigpio.OUTPUT)
-		self.set_mode(GREEN_PIN, pigpio.OUTPUT)
-		self.set_mode(BLUE_PIN, pigpio.OUTPUT)
-		self.set_mode(PWM_PIN, pigpio.INPUT)
-		self.set_pull_up_down(PWM_PIN, 2)
+		self.gpio = pigpio.pi()
+		if self.gpio.connected:
+			print("Tank successfully initialized")
+		else:
+			print("[Error] PiGPIO is not running")
+			quit()
+		self.gpio.set_mode(RED_PIN, pigpio.OUTPUT)
+		self.gpio.set_mode(GREEN_PIN, pigpio.OUTPUT)
+		self.gpio.set_mode(BLUE_PIN, pigpio.OUTPUT)
+		self.gpio.set_mode(PWM_PIN, pigpio.INPUT)
+		self.gpio.set_pull_up_down(PWM_PIN, 2)
 
 	def set(self, color):
 		r, g, b = color
-		pi.write(RED_PIN, r)
-		pi.write(GREEN_PIN, g)
-		pi.write(BLUE_PIN, b)
+		self.gpio.write(RED_PIN, r)
+		self.gpio.write(GREEN_PIN, g)
+		self.gpio.write(BLUE_PIN, b)
