36639598045db1b6be0538582ad16666ab42ee75
Levi Lesches <levilesches@gmail.com>
2023-05-09 10:53:44 -0400

    Better init logic

--- lib/leds.py
+++ lib/leds.py
@@ -14,19 +14,23 @@ class Leds:
 	BLUE_PIN = 26
 	PWM_PIN = 17
 
-	def __init__(self): 
+	def init_pigpiod(self): 
+		self.gpio = pigpio.pi()
+		if self.gpio.connected: return
 		system("sudo pigpiod")
 		time.sleep(1)
-		self.gpio = pigpio.pi()
-		if not self.gpio.connected:
-			print("[Error] PiGPIO is not running")
-			quit()
-		print("Tank successfully initialized")
+		if not self.gpio.connected: 
+			print("[Error] Could not initialize pigpiod")
+			quit()		
+
+	def __init__(self): 
+		self.init_pigpiod()
 		self.gpio.set_mode(RED_PIN, pigpio.OUTPUT)
 		self.gpio.set_mode(GREEN_PIN, pigpio.OUTPUT)
 		self.gpio.set_mode(BLUE_PIN, pigpio.OUTPUT)
 		self.gpio.set_mode(PWM_PIN, pigpio.INPUT)
 		self.gpio.set_pull_up_down(PWM_PIN, 2)
+		print("Initialized LED strip")
 
 	def set(self, color):
 		r, g, b = color
