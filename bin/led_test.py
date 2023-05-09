c8b10da424396b6022e600cae650c9f6642c6dd6
Levi Lesches <levilesches@gmail.com>
2023-05-09 11:56:57 -0400

    Working test script

--- bin/led_test.py
+++ bin/led_test.py
@@ -1,7 +1,7 @@
 import time
 
 from network import ProtoSocket, Device
-from network.generated import UpdateSetting, Color
+from network.generated import UpdateSetting, Color, Connect
 from lib.leds import *
 
 socket = ProtoSocket(port=8000, device=Device.DASHBOARD, destination=("127.0.0.1", 8001))
@@ -13,9 +13,11 @@ custom = Color(red=0.75, green=0, blue=0.25)
 
 if __name__ == '__main__':
 	socket.send_message(UpdateSetting(color=red))
-	time.sleep(0.25)
-	socket.send_message(UpdateSetting(color=red))
-	time.sleep(0.25)
-	socket.send_message(UpdateSetting(color=red))
-	time.sleep(0.25)
+	time.sleep(0.2)
+	socket.send_message(UpdateSetting(color=blue))
+	time.sleep(0.2)
+	socket.send_message(UpdateSetting(color=green))
+	time.sleep(0.2)
 	socket.send_message(UpdateSetting(color=custom))
+	time.sleep(0.2)
+	socket.send_message(UpdateSetting(color=None))
