db64b77c9690d0b09ab5cc5e9d9f09a1fcbf5a7e
Levi Lesches <levilesches@gmail.com>
2023-05-09 11:52:49 -0400

    Updated test script

--- bin/led_test.py
+++ bin/led_test.py
@@ -5,6 +5,7 @@ from network.generated import UpdateSetting, Color
 from lib.leds import *
 
 socket = ProtoSocket(port=8000, device=Device.DASHBOARD, destination=("127.0.0.1", 8001))
+socket.send_message(Connect(sender=Device.DASHBOARD, receiver=Device.SUBSYSTEMS))
 red = Color(red=1, green=0, blue=0)
 green = Color(red=0, green=1, blue=0)
 blue = Color(red=0, green=0, blue=1)
@@ -12,9 +13,9 @@ custom = Color(red=0.75, green=0, blue=0.25)
 
 if __name__ == '__main__':
 	socket.send_message(UpdateSetting(color=red))
-	time.sleep(1)
+	time.sleep(0.25)
 	socket.send_message(UpdateSetting(color=red))
-	time.sleep(1)
+	time.sleep(0.25)
 	socket.send_message(UpdateSetting(color=red))
-	time.sleep(1)
+	time.sleep(0.25)
 	socket.send_message(UpdateSetting(color=custom))
